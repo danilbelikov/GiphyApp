cfefc370c63e8876a6ac392fdb6101e3c8fbd28f
danilbelikov <danilkikaoskol@mail.ru>
2023-03-05 16:17:11 +0300

    Part-final-all_1

--- app/src/main/java/com/example/gipghyapp/utils/Constants.kt
+++ app/src/main/java/com/example/gipghyapp/utils/Constants.kt
@@ -3,7 +3,6 @@ package com.example.gipghyapp.utils
 class Constants {
     companion object {
         const val BASE_URL = "https://api.giphy.com/"
-        //const val API_KEY = "Do4NnwLBABhq1ArW32E6SnImzKuLf98I"
         const val API_KEY = "UxxuxifTFeN04SMGVA9uhKhcStSyarcq"
     }
 }
